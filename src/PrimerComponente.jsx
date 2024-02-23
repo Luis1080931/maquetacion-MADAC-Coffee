97bd1cf01db779c265f4e0625e2ba4742806b0e7
Luis1080931 <luisu0616@gmail.com>
2024-02-23 16:39:13 -0500

    primerComponent

---
+++ src/PrimerComponente.jsx
@@ -0,0 +1,7 @@
+function PrimerComponent() {
+    return(
+        <h1>Hola mundo</h1>
+    )
+}
+
+export default PrimerComponent
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import PrimerComponent from './PrimerComponente.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <PrimerComponent />
   </React.StrictMode>,
 )
